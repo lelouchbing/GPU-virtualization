2a905b1a4c408c44fede3eca9c2e9a3d7b3d8418
lelouchbing <lelouchbing@gmail.com>
2016-03-24 08:28:18 -0400

    First commit

---
+++ backend/Memmap.h
@@ -0,0 +1,10 @@
+#ifndef MEMMAP 
+#define MEMMAP 0
+
+#include <vector>
+#include <map>
+
+using namespace std;
+extern map<size_t, char*> memmap;
+static bool delay = false;
+#endif
---
+++ make.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+make clean
+sudo make install
